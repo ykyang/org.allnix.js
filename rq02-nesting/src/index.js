f45f5fbb31f86891c62d925f2ffd54f444f25eab
Yi-Kun Yang <ykyang@gmail.com>
2023-06-25 13:06:00 -0500

    rq02-siblings-fragment

--- rq02-nesting/src/index.js
+++ rq02-nesting/src/index.js
@@ -4,7 +4,8 @@ import ReactDOM from "react-dom/client";
 const world = React.createElement("em", null, "world")
 const title = React.createElement("h1", null, ["Hello ", world, "!"]);
 const link = React.createElement("a", {href: "//reactjs.org"}, "Read more")
-const group = React.createElement("div", null, title, link)
+//const group = React.createElement("div", null, title, link)
+const group = React.createElement(React.Fragment, null, title, link)
 const domElement = document.getElementById("root");
 //ReactDOM.createRoot(domElement).render(title);
 ReactDOM.createRoot(domElement).render(group);
