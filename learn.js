e567247e06f6e33ade6634de33178d545b9e792b
Yi-Kun Yang <ykyang@gmail.com>
2023-07-05 17:35:52 -0500

    rq 5.2.6

--- learn.js
+++ learn.js
@@ -1,1 +1,11 @@
 console.log("Hello World!")
+
+function learn_map() {
+    todos = ["Feed the plants", "Water the dishes"]
+
+    todos.map((todo,index) => (
+        console.log(`${index} ${todo}`)
+    ))
+}
+
+
